ca247bb9a8c68081c419498b776a97cdba44b212
morfeii <morfeii88@gmail.com>
2020-03-11 08:55:32 +0200

    Updates

---
+++ precache-manifest.204aed40482debcbdc9174117064ba66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee39e8a3b0480e99204f2d76c7b00d22",
+    "url": "./index.html"
+  },
+  {
+    "revision": "abefba6a1e2ce878d3c8",
+    "url": "./static/css/2.aeed1b96.chunk.css"
+  },
+  {
+    "revision": "26164a734b526dd55ecd",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "abefba6a1e2ce878d3c8",
+    "url": "./static/js/2.18aa6171.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "./static/js/2.18aa6171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26164a734b526dd55ecd",
+    "url": "./static/js/main.2d54e74b.chunk.js"
+  },
+  {
+    "revision": "950315c42b3e0eca8ca2",
+    "url": "./static/js/runtime-main.6df99021.js"
+  }
+]);
